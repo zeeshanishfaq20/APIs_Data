8b07e2b6c867c6784429dc6ac2987f9eee33169e
zeeshanishfaq20 <zeeshan20@gmail.com>
2024-12-31 00:14:07 +0500

    initial commit

---
+++ snb.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+from google.cloud import bigquery
+import os
+# Make a GET request to the API
+response = requests.get("https://randomuser.me/api")
+data = response.json()
+
+# Extract the 'results' key
+results_data = data['results']
+
+# Normalize the JSON data
+df = pd.json_normalize(results_data)
+
+# Extract the first and last names
+df['Full Name'] = df['name.title'] + ' ' + df['name.first'] + ' ' + df['name.last']
+
+names_df = df[['Full Name']]
+# Select only the relevant column
+
+credentials_path = r"C:\Users\block360_\Desktop\Zeeshan Data\Project\bubbly-enigma-444309-h2-88b72ab71a0d.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+client = bigquery.Client()
+
+table_id = "bubbly-enigma-444309-h2.Shops.name"
+
+job = client.load_table_from_dataframe(names_df, table_id)
+
+job.result()
+
+print(f"Data successfully loaded into BigQuery table {table_id}")
